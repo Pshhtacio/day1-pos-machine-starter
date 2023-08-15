bbe1e9732d85d410056050cbf8253f15ceeb68d1
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-15 23:32:46 +0800

    feat: Add method definition for decodeToItem

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,14 +1,37 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static pos.machine.ItemsLoader.loadAllItems;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        List<ReceiptItem> items = decodeToItem(barcodes);
+
         return null;
     }
 
     public List<ReceiptItem> decodeToItem(List<String> barcodes) {
-        return null;
+        List<Item> items = loadAllItems();
+        Map<String, Long> barcodeCountMap = barcodes.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        List<ReceiptItem> receiptItems = new ArrayList<>();
+
+        for (Item item : items) {
+            String barcode = item.getBarcode();
+            Long count = barcodeCountMap.get(barcode);
+
+            if (count != null && count > 0) {
+                receiptItems.add(new ReceiptItem(item.getName(), item.getPrice(), count.intValue()));
+            }
+        }
+
+        return receiptItems;
     }
 
     public Receipt calculateCost(List<ReceiptItem> receiptItem) {
